be61ec3d4c9ab176745a27f820db5b8f62651d6e
Douma <douma0898@gmail.com>
2026-08-04 11:17:49 +0000

    increase vite chunk warning limit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
